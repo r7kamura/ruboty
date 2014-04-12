e7c6110027e3861d206f79ff67581bbaeb4de5e5
Ryo Nakamura <r7kamura@gmail.com>
2014-04-12 15:58:08 +0900

    Implement the true behavior of ./bin/ellen --generate

--- lib/ellen/commands/generate.rb
+++ lib/ellen/commands/generate.rb
@@ -1,8 +1,17 @@
+require "fileutils"
+
+# Generate ./ellen/ directory from our templates.
 module Ellen
   module Commands
     class Generate
       def call
-        puts "#{self.class}##{__method__}"
+        FileUtils.cp_r(templates_directory_path, "./ellen")
+      end
+
+      private
+
+      def templates_directory_path
+        File.expand_path("../../../../templates", __FILE__)
       end
     end
   end
