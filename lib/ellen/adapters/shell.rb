924d2e1874eaf9cf41bdf49ae582a187e576c177
Ryo Nakamura <r7kamura@gmail.com>
2014-04-12 19:28:11 +0900

    Implement simple shell behavior

--- lib/ellen/adapters/shell.rb
+++ lib/ellen/adapters/shell.rb
@@ -3,8 +3,31 @@ module Ellen
     class Shell < Base
       register :shell
 
-      # TODO
       def run
+        explain
+        loop { step }
+      end
+
+      private
+
+      def explain
+        puts "Type `exit` or `quit` to end the session."
+      end
+
+      def prompt
+        print "> "
+      end
+
+      def read
+        gets.chomp
+      end
+
+      def step
+        prompt
+        case read
+        when "exit", "quit"
+          exit
+        end
       end
     end
   end
