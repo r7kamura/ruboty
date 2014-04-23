a18657b1d4a1b13b8b30370a0dfbe150400373ab
Ryo Nakamura <r7kamura@gmail.com>
2014-04-24 01:09:31 +0900

    Remove old comment

--- lib/ellen/adapters/base.rb
+++ lib/ellen/adapters/base.rb
@@ -1,14 +1,4 @@
 # Abstract class to be inherited from adapter class.
-#
-# Example:
-#
-#   # Used as `ellen --adapter=my_adapter`
-#   class MyAdapter < Ellen::Adapters::Base
-#     def run
-#       ... do your work ...
-#     end
-#   end
-#
 module Ellen
   module Adapters
     class Base
