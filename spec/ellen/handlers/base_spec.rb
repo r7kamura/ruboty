54df40bd2eb7c76605f82e0e73d4b48b22b6b803
Ryo Nakamura <r7kamura@gmail.com>
2014-04-24 00:54:19 +0900

    Write spec for uncommon handler

---
+++ spec/ellen/handlers/base_spec.rb
@@ -0,0 +1,28 @@
+require "spec_helper"
+
+describe Ellen::Handlers::Base do
+  after do
+    Ellen.handlers.pop
+  end
+
+  let(:robot) do
+    Ellen::Robot.new
+  end
+
+  let!(:handler_class) do
+    Class.new(described_class) do
+      on(/(\d+) \+ (\d+)/, name: "addition", all: true)
+
+      def addition(message)
+        robot.say(message[1].to_i + message[2].to_i)
+      end
+    end
+  end
+
+  describe ".on" do
+    it "registers an action to the handler" do
+      robot.should_receive(:say).with(2)
+      robot.receive(body: "1 + 1")
+    end
+  end
+end
