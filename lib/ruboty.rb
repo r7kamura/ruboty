c484779a39a49d96d1436af3aed55e7e0be3d872
Ryo Nakamura <r7kamura@gmail.com>
2015-02-16 03:54:45 +0900

    Enable sync option of stdout to prevent buffering

--- lib/ruboty.rb
+++ lib/ruboty.rb
@@ -12,6 +12,7 @@ module Ruboty
 
     def logger
       @logger ||= begin
+        $stdout.sync = true
         logger = Ruboty::Logger.new($stdout)
         logger.level = ENV["LOG_LEVEL"] ? ENV["LOG_LEVEL"].to_i : Logger::ERROR
         logger
