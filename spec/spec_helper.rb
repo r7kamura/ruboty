561fd8a5b2ed4d3b372eb2b3c08479f06ba8f8ac
r7kamura <r7kamura@gmail.com>
2016-11-27 14:41:49 +0900

    Remove codeclimate from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,6 @@
 require "simplecov"
 SimpleCov.start
 
-if ENV["CI"]
-  require "codeclimate-test-reporter"
-  CodeClimate::TestReporter.start
-end
-
 require "active_support/core_ext/string/strip"
 require "ruboty"
 
